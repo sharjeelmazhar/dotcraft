ce10e0434dad659fcfa587322fa19cc9d05debfe
Sharjeel M. <73254881+sharjeelmazhar@users.noreply.github.com>
2024-09-29 17:54:38 +0500

    updated nvim lspconfig and debugger

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,23 +1,28 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright", "bashls", "clangd", "yamlls", "ruff_lsp" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-		end,
-	},
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "pyright", "bashls", "clangd", "yamlls", "ruff_lsp" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.pyright.setup({})
+            lspconfig.bashls.setup({})
+            lspconfig.clangd.setup({})
+            lspconfig.yamlls.setup({})
+            lspconfig.ruff_lsp.setup({})
+        end,
+    },
 }
