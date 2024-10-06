2462e0c320320430aa0cef93c4196a9128c439f0
Sharjeel M. Rajput <sharjeelmazhar@gmail.com>
2024-10-06 17:11:13 +0500

    Integrated lazygit in neovim

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -32,7 +32,7 @@ vim.keymap.set("v", "<Right>", "<Nop>", { desc = "Disable Right arrow key" })
 -- Telescope
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find files using Telescope" })
 vim.keymap.set("n", "<leader>of", ":Telescope oldfiles<CR>", { desc = "Find old files using Telescope" })
-vim.keymap.set("n", "<leader>lg", ":Telescope live_grep<CR>", { desc = "Live grep using Telescope" })
+vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Live grep using Telescope" })
 vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "List buffers using Telescope" })
 vim.keymap.set("n", "<leader>fht", ":Telescope help_tags<CR>", { desc = "List help tags using Telescope" })
 vim.keymap.set("n", "<leader>lp", ":Telescope lsp_document_symbols<CR>", { desc = "Show LSP document symbols" })
@@ -62,24 +62,24 @@ vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { desc = "Navigate u
 vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { desc = "Navigate right in Tmux" })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function()
-		vim.lsp.buf.format()
-	end,
+    pattern = "*",
+    callback = function()
+        vim.lsp.buf.format()
+    end,
 })
 
 -- Indent selected lines and keep visual mode active
 vim.api.nvim_set_keymap(
-	"v",
-	"<",
-	"<gv",
-	{ noremap = true, silent = true, desc = "Indent left and stay in visual mode" }
+    "v",
+    "<",
+    "<gv",
+    { noremap = true, silent = true, desc = "Indent left and stay in visual mode" }
 )
 vim.api.nvim_set_keymap(
-	"v",
-	">",
-	">gv",
-	{ noremap = true, silent = true, desc = "Indent right and stay in visual mode" }
+    "v",
+    ">",
+    ">gv",
+    { noremap = true, silent = true, desc = "Indent right and stay in visual mode" }
 )
 
 -- Move to the next buffer
@@ -87,10 +87,10 @@ vim.api.nvim_set_keymap("n", "<TAB>", ":bnext<CR>", { noremap = true, silent = t
 
 -- Move to the previous buffer
 vim.api.nvim_set_keymap(
-	"n",
-	"<S-TAB>",
-	":bprevious<CR>",
-	{ noremap = true, silent = true, desc = "Move to previous buffer" }
+    "n",
+    "<S-TAB>",
+    ":bprevious<CR>",
+    { noremap = true, silent = true, desc = "Move to previous buffer" }
 )
 
 -- Toggle terminal
@@ -108,10 +108,10 @@ vim.keymap.set("x", "<leader>p", '"_dp', { desc = "Paste without overwriting reg
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
 vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from system clipboard" })
 vim.keymap.set(
-	"n",
-	"<leader>s",
-	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-	{ desc = "Search and replace current word" }
+    "n",
+    "<leader>s",
+    [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    { desc = "Search and replace current word" }
 )
 
 -- Keybindings for vim-test
@@ -123,12 +123,12 @@ vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", { desc = "Visit last test run
 
 -- Keybinding to run with verbose flag
 vim.keymap.set("n", "<leader>va", function()
-	-- Temporarily set the pytest options to include -v
-	vim.cmd("let test#python#pytest#options = '-v'")
-	-- Run the test suite
-	vim.cmd("TestSuite")
-	-- Reset the pytest options after running
-	vim.cmd("let test#python#pytest#options = ''")
+    -- Temporarily set the pytest options to include -v
+    vim.cmd("let test#python#pytest#options = '-v'")
+    -- Run the test suite
+    vim.cmd("TestSuite")
+    -- Reset the pytest options after running
+    vim.cmd("let test#python#pytest#options = ''")
 end, { desc = "Run test suite with verbose output" })
 
 vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
---
+++ nvim/.config/nvim/lua/plugins/nekifoch.lua
@@ -0,0 +1,6 @@
+return {
+	"NeViRAIDE/nekifoch.nvim",
+	build = "chmod +x ./install.sh && ./install.sh",
+	cmd = "Nekifoch",
+	config = true,
+}
